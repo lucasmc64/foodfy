0c2e1e12c42fd83bec63fffe42bef3e7db9c6c2d
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-07-12 10:31:52 -0300

    Renaming for better understanding

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 
-const recipesController = require('./app/controllers/RecipesController')
-const adminController = require('./app/controllers/AdminController')
-const adminChefsController = require('./app/controllers/AdminChefsController')
+const recipesController = require('./app/controllers/recipes')
+const adminRecipesController = require('./app/controllers/adminRecipes')
+const adminChefsController = require('./app/controllers/adminChefs')
 
 const routes = express.Router()
 
@@ -11,13 +11,13 @@ routes.get('/about', recipesController.about) // Sobre o Foodfy
 routes.get('/recipes', recipesController.recipes) // Todas as comidas que tem receita
 routes.get('/recipes/:id', recipesController.recipe) // Receita de comida específica
 
-routes.get('/admin/recipes', adminController.recipes) // Mostrar a lista de receitas
-routes.get('/admin/recipes/create', adminController.create) // Mostrar formulário de nova receita
-routes.post('/admin/recipes', adminController.post) // Cadastrar nova receita
-routes.get('/admin/recipes/:id', adminController.show) // Exibir detalhes de uma receita
-routes.get('/admin/recipes/:id/edit', adminController.edit) // Mostrar formulário de edição de receita
-routes.put('/admin/recipes', adminController.put) // Editar uma receita
-routes.delete('/admin/recipes', adminController.delete) // Deletar uma receita
+routes.get('/admin/recipes', adminRecipesController.recipes) // Mostrar a lista de receitas
+routes.get('/admin/recipes/create', adminRecipesController.create) // Mostrar formulário de nova receita
+routes.post('/admin/recipes', adminRecipesController.post) // Cadastrar nova receita
+routes.get('/admin/recipes/:id', adminRecipesController.show) // Exibir detalhes de uma receita
+routes.get('/admin/recipes/:id/edit', adminRecipesController.edit) // Mostrar formulário de edição de receita
+routes.put('/admin/recipes', adminRecipesController.put) // Editar uma receita
+routes.delete('/admin/recipes', adminRecipesController.delete) // Deletar uma receita
 
 routes.get('/admin/chefs', adminChefsController.index) // Listagem de chefs
 routes.get('/admin/chefs/create', adminChefsController.create) // Mostrar formulário de novo chef
