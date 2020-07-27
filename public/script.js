ed6ebd80e96be5a3341e02e2bd5a0609d1c1fc68
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-07-27 12:22:58 -0300

    Functions of the admin part of the site are now in the global script

--- public/script.js
+++ public/script.js
@@ -9,3 +9,24 @@ function hideAndShow(buttonToHideAndShow) {
         buttonToHideAndShow.textContent = 'Esconder';
     }
 }
+
+function addInput(button) {
+    // Procura pela div a adicionar elementos
+    let div = window.document.querySelector(`.${button.id}-div`);
+
+    let fields = div.querySelectorAll('input');
+
+    // Realiza um clone do último item adicionado
+    const newField = fields[fields.length - 1].cloneNode(true);
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.value == '') return false;
+
+    // Deixa o valor do input vazio
+    newField.value = '';
+    div.appendChild(newField);
+
+    // Foca no campo recém criado
+    fields = div.querySelectorAll('input');
+    fields[fields.length - 1].focus();
+}
