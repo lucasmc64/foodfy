2cab285df22e1e9fc483eef53da0de0b4ba2b3a3
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-12-28 21:36:46 -0300

    Corrections and 2 routes created

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const routes = express.Router()
 
-/*
+const UserController = require('../app/controllers/user')
+
 // Rotas de perfil de um usuário logado
-routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
-routes.put('/profile', ProfileController.put)// Editar o usuário logado
+//routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
+//routes.put('/profile', ProfileController.put) // Editar o usuário logado
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-routes.get('/users', UserController.list) //Mostrar a lista de usuários cadastrados
-routes.post('/users', UserController.post) //Cadastrar um usuário
-routes.put('/users', UserController.put) // Editar um usuário
-routes.delete('/users', UserController.delete) // Deletar um usuário
-*/
+routes.get('/users', UserController.index) // Listagem de usuários cadastrados
+routes.get('/users/create', UserController.create) // Mostrar formulário de novo usuário
+//routes.post('/users', UserController.post) // Cadastrar um usuário
+//routes.put('/users', UserController.put) // Editar um usuário
+//routes.delete('/users', UserController.delete) // Deletar um usuário
 
 module.exports = routes
