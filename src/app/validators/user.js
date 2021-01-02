3256a63baec00ba35b593bdfb10a33cdccd4ef28
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-02 20:54:49 -0300

    Update validator

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,3 +1,4 @@
+const { update } = require('../controllers/user')
 const User = require('../models/User')
 
 async function post(request, response, next) {
@@ -36,6 +37,34 @@ async function post(request, response, next) {
     next()
 }
 
+async function put(request, response, next) {
+    let { password, password_repeat } = request.body
+
+    console.log(request.body)
+    const keys = Object.keys(request.body)
+
+    for(key of keys) {
+        if(request.body[key] == '') {
+            return response.render('admin/users/create', {
+                users_page: true,
+                user: request.body,
+                error: 'Por favor, preencha todos os campos.'
+            })
+        }
+    }
+
+    if(password !== password_repeat) {
+        return response.render('admin/users/create', {
+            users_page: true,
+            user: request.body,
+            error: 'A senha e a repetição de senha estão diferentes.'
+        })
+    }
+    
+    next()
+}
+
 module.exports = {
-    post
+    post,
+    put
 }
