222ac096a1721dfb0bc8a6c7155d3fc7939700c0
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-02 14:01:34 -0300

    User validator

---
+++ src/app/validators/user.js
@@ -0,0 +1,41 @@
+const User = require('../models/User')
+
+async function post(request, response, next) {
+    let { email, password, password_repeat } = request.body
+
+    const keys = Object.keys(request.body)
+
+    for(key of keys) {
+        if(request.body[key] == '') {
+            return response.render('admin/users/create', {
+                users_page: true,
+                user: request.body,
+                error: 'Por favor, preencha todos os campos.'
+            })
+        }
+    }
+
+    let results = await User.findByEmail(email)
+
+    if(results.rows.length != 0) {
+        return response.render('admin/users/create', {
+            users_page: true,
+            user: request.body,
+            error: 'Já existe um usuário cadastrado com este email.'
+        })
+    }
+
+    if(password !== password_repeat) {
+        return response.render('admin/users/create', {
+            users_page: true,
+            user: request.body,
+            error: 'A senha e a repetição de senha estão diferentes.'
+        })
+    }
+
+    next()
+}
+
+module.exports = {
+    post
+}
