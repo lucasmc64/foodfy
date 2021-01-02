5c464ef0d2f29367f998408257e675101f04b99d
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-02 14:00:03 -0300

    The user listing is now dynamic. Two new functions added.

--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -1,12 +1,32 @@
+const { use } = require('../../routes/users')
+const User = require('../models/User')
+
 module.exports = {
-    index(request, response) {
+    async index(request, response) {
+        const users = await User.all()
+
         return response.render('admin/users/index', {
+            users: users.rows,
             users_page: true
         })
     },
 
     create(request, response) {
         return response.render('admin/users/create', {
+            users_page: true
+        })
+    },
+
+    async post(request, response) {
+        const user_id = await User.create(request.body)
+
+        request.session.user_id = user_id 
+
+        return response.redirect('/admin/users')
+    },
+
+    edit(request, response) {
+        return response.render('admin/users/edit', {
             users_page: true
         })
     }
