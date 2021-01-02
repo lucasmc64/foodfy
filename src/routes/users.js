387a144cb98cf08e949fbb3f572a0da899602571
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-02 14:02:08 -0300

    New route and added user validator

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,8 @@ const routes = express.Router()
 
 const UserController = require('../app/controllers/user')
 
+const UserValidator = require('../app/validators/user')
+
 // Rotas de perfil de um usuário logado
 //routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
 //routes.put('/profile', ProfileController.put) // Editar o usuário logado
@@ -10,7 +12,8 @@ const UserController = require('../app/controllers/user')
 // Rotas que o administrador irá acessar para gerenciar usuários
 routes.get('/users', UserController.index) // Listagem de usuários cadastrados
 routes.get('/users/create', UserController.create) // Mostrar formulário de novo usuário
-//routes.post('/users', UserController.post) // Cadastrar um usuário
+routes.get('/users/:id/edit', UserController.edit) // Mostrar formulário de edição de usuário
+routes.post('/users', UserValidator.post, UserController.post) // Cadastrar um usuário
 //routes.put('/users', UserController.put) // Editar um usuário
 //routes.delete('/users', UserController.delete) // Deletar um usuário
 
