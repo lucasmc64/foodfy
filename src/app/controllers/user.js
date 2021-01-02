a3b0124eaacee405f61c3d01cf173489d1c64d57
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-02 20:54:00 -0300

    Improved user page controller and added 'put' function

--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -18,15 +18,37 @@ module.exports = {
     },
 
     async post(request, response) {
-        const user_id = await User.create(request.body)
+        const user = await User.create(request.body)
 
-        request.session.user_id = user_id 
+        request.session.user = user 
 
         return response.redirect('/admin/users')
     },
 
-    edit(request, response) {
+    async edit(request, response) {
+        const user_id = request.params.id
+        let current_user = request.session.user
+
+        current_user = (await User.findById(current_user.rows[0].id)).rows[0]
+        let user = (await User.findById(user_id)).rows[0]
+
+        if(user_id !== current_user.id && !current_user.is_admin) {
+            return response.redirect('admin/users', {
+                error: 'Você não tem permissão para editar esse usuário.'
+            })
+        }
+
         return response.render('admin/users/edit', {
+            user: user,
+            users_page: true
+        })
+    },
+    async put(request, response) {
+        const user = await User.update(request.body)
+        console.log(user.rows[0])
+
+        return response.render('admin/users/edit', {
+            user: user.rows[0],
             users_page: true
         })
     }
