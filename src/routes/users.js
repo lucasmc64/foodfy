5fe5df3df56f8b2fb6ef3693bd310888f36b72a2
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-01-08 12:34:26 -0300

    Added new user routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,19 +2,30 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('../app/controllers/user')
-
 const UserValidator = require('../app/validators/user')
 
-// Rotas de perfil de um usuário logado
-//routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
-//routes.put('/profile', ProfileController.put) // Editar o usuário logado
+const SessionController = require('../app/controllers/session')
+const SessionValidator = require('../app/validators/session')
+
+const { redirectToAdminDashboard, onlyUsersCanAccess } = require('../app/middlewares/session')
+
+// Login / Logout
+routes.get('/', redirectToAdminDashboard, SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
+
+// Reset password / Forgot password
+routes.get('/forgot-password', SessionController.forgotPasswordForm)
+routes.post('/forgot-password', SessionValidator.forgotPassword, SessionController.forgotPassword)
+routes.get('/reset-password', SessionController.resetPasswordForm)
+routes.post('/reset-password', SessionValidator.resetPassword, SessionController.resetPassword)
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-routes.get('/users', UserController.index) // Listagem de usuários cadastrados
-routes.get('/users/create', UserController.create) // Mostrar formulário de novo usuário
-routes.get('/users/:id/edit', UserController.edit) // Mostrar formulário de edição de usuário
+routes.get('/users', onlyUsersCanAccess, UserController.index) // Listagem de usuários cadastrados
+routes.get('/users/create', onlyUsersCanAccess, UserController.create) // Mostrar formulário de novo usuário
 routes.post('/users', UserValidator.post, UserController.post) // Cadastrar um usuário
+routes.get('/users/:id/edit', onlyUsersCanAccess, UserController.edit) // Mostrar formulário de edição de usuário
 routes.put('/users', UserValidator.put, UserController.put) // Atualizar um usuário
-//routes.delete('/users', UserController.delete) // Deletar um usuário
+routes.delete('/users', UserController.delete) // Deletar um usuário
 
 module.exports = routes
